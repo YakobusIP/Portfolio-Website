b3e0ab9fbe36a20aa24ef73d2fbac33ed9c91cca
YakobusIP <82079465+YakobusIP@users.noreply.github.com>
2023-05-04 21:37:02 +0700

    update home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const myImage = ref<HTMLImageElement>();
+
+onMounted(() => {
+    myImage.value?.addEventListener("load", () => {
+        setTimeout(() => {
+            myImage.value?.classList.remove("opacity-0");
+            myImage.value?.classList.add("animate-image-in");
+        }, 100);
+    });
+});
+</script>
 
 <template>
-    <div class="relative flex flex-1 items-center justify-center">
-        <div class="flex flex-col text-white text-center px-64">
+    <div class="relative flex flex-1 items-center justify-center px-32 gap-x-8">
+        <div class="flex flex-col text-white w-2/3">
             <h4
-                class="text-3xl overflow-hidden animate-hello-typing whitespace-nowrap m-auto"
+                class="text-3xl overflow-hidden animate-hello-typing whitespace-nowrap"
             >
                 Hello, my name is
             </h4>
@@ -25,5 +38,12 @@
                 bits.
             </div>
         </div>
+
+        <img
+            loading="lazy"
+            ref="myImage"
+            src="Transparent Foto-min.png"
+            class="w-[25rem] h-[37.5rem] opacity-0"
+        />
     </div>
 </template>
